fbd80c154268d35815121820ec185fbce816fd98
KephM <kephryn.merkley@tooeleschools.org>
2019-09-04 10:07:59 -0600

    test project pushing to git

---
+++ identify_yourself/src/identify_yourself_kephryn/Schedule.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package identify_yourself_kephryn;
+
+/**
+ * @author kephryn.merkley
+ * Identify yourself
+ * 9-4-19
+ */
+public class Schedule {
+	
+	public Schedule() {
+		// this is my blue day schedule from first to fifth period
+		System.out.println("Kephryn Merkley");
+		System.out.println("Blue day schedule");
+		System.out.println("Blue day | no first period");
+		System.out.println("Blue day | no second period");
+		System.out.println("Blue day | no third period");
+		System.out.println("Blue day | Programming Python");
+		System.out.println("Blue day | Programming Python");
+		
+		//this is my silver day schedule from first to fifth period
+		System.out.println("Silver day schedule");
+		System.out.println("Silver day | Mobile App Development");
+		System.out.println("Silver day | Mobile App Development");
+		System.out.println("Silver day | Digital Arts Intro");
+		System.out.println("Silver day | Credit Recovery");
+		System.out.println("Silver day | no fifth period");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		new Schedule();
+	}
+
+}
---
+++ test_project/src/test_project_Kephryn/Hello_world.java
@@ -0,0 +1,25 @@
+package test_project_Kephryn;
+
+/*
+ * Kephryn Merkley
+ * Hello world
+ * 9-4-19
+ */
+
+public class Hello_world {
+	/**
+	 * 
+	 */
+	public Hello_world() {
+		System.out.println("hello world");
+	}
+	
+	/**
+	 * 
+	 * @param args
+	 */
+		public static void main(String[] args) {
+			
+	}
+
+}
